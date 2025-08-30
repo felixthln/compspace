9727cfe59798dc6f461049249872da42604441e0
Felix Thelen <felix.thelen@ruhr-uni-bochum.de>
2025-08-30 21:48:18 +0200

    Fix of doc errors (10)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,8 @@ from pathlib import Path
 
 # Get the repository root directory
 repo_root = Path(__file__).parents[2]
-print('Root directory: ' + str(repo_root))
+print('Root directory: ' + str(list(Path(__file__).parents[4].iterdir())))
+
 # Add the source directory to sys.path
 sys.path.insert(0, str(repo_root / 'src'))
 
